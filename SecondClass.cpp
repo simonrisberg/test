d2619dea3dcfd6cbacb796ebb94be7c995ce7c36
Simon Risberg <simon@evello.org>
2016-02-04 10:47:14 +0100

    Lade till ny skit

---
+++ SecondClass.cpp
@@ -0,0 +1,14 @@
+#include "SecondClass.hpp"
+#include <iostream>
+using namespace std;
+
+SecondClass::SecondClass(){
+    cout << "Jag är en banan" << endl;
+    
+}
+
+    
+
+
+
+
---
+++ SecondClass.hpp
@@ -0,0 +1,17 @@
+
+
+#ifndef SecondClass_hpp
+#define SecondClass_hpp
+
+#include <stdio.h>
+
+class SecondClass{
+    
+    public:
+        SecondClass();
+        
+    private:
+    protected:
+};
+
+#endif
